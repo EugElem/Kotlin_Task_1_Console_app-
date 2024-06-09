b6ec5dcbd2c73d6d79c1c79d2bff64c4d3c42e08
Tirex <e.elembaev@yandex.ru>
2024-06-09 21:00:58 +0500

    first commit

---
+++ src/Task1/Domain/UseCase/AddEmployee.kt
@@ -0,0 +1,128 @@
+package Task1.Domain.UseCase
+
+import Task1.Data.EmployeeReposytory
+import Task1.Domain.Models.Employee
+
+class AddEmployee(
+    firstName: String? =null,
+    lastName:String? =null,
+    patronymic:String? =null,
+    position: String? =null,
+    department: String? =null,
+    urlPhoto: String? =null,
+    birthDate: String? =null,
+    startDate: String? =null,
+    )
+    : Employee(
+    firstName,
+    lastName,
+    patronymic,
+    position,
+    department,
+    urlPhoto,
+    birthDate,
+    startDate,) {
+
+
+
+
+    /*override fun showAll(){
+        var i:Int=1
+
+        println("""
+            --------------------------------------------------------------------------------------------------------------
+            |Фамилия         |Имя             |Отчество        |Дата род.  |Должность       |Ссылка на фото  |Дата начала|
+             ${lastName}      $firstName       $patronymic      $birthDate  $department      $position        $startDate
+        """.trimIndent())
+
+    }*/
+    //Сообщить, что ввести пользователю
+    //Получить введеннные данные
+
+    //Сохранить
+
+
+    // Сохранение (добавление в Лист)
+    fun addNewEmployee(listEmployee: EmployeeReposytory) {
+        var employee:Employee = Employee(
+            firstName,
+            lastName,
+            patronymic,
+            position,
+            department,
+            urlPhoto,
+            birthDate,
+            startDate,
+            )
+
+
+        listEmployee.listEmployee.add(employee)
+        listEmployee.printList()
+    }
+
+    // вызов фушкций получения от пользователя параметров гового сотрудника
+    fun getParametresNewEmployee(){
+        getLastName()
+        getName()
+        getРatronymic()
+        getBirthDate()
+        getPosition()
+        getDepartment()
+        getUrlPhoto()
+        getStartDate()
+    }
+
+
+    fun getLastName(){
+        print("Введите фамилию: ")
+        lastName = readln()
+        lastName +=" ".repeat(15-lastName!!.length)
+    }
+
+    fun getName(){
+        print("Введите имя: ")
+        firstName = readln()
+        firstName +=" ".repeat(15-firstName!!.length)
+    }
+
+    fun getРatronymic(){
+        print("Введите отчество, при наличии: ")
+        patronymic = readln()
+        patronymic +=" ".repeat(15-patronymic!!.length)
+
+    }
+
+    fun getBirthDate(){
+        print("Введите дату рождения: ")
+        birthDate = readln()
+        birthDate +=" ".repeat(10-birthDate!!.length)
+
+    }
+
+    fun getPosition(){
+        print("Введите должность: ")
+        position = readln()
+        position +=" ".repeat(15-position!!.length)
+
+    }
+
+    fun getDepartment(){
+        print("Введите отдел: ")
+        department = readln()
+        department +=" ".repeat(15-department!!.length)
+
+    }
+
+    fun getUrlPhoto(){
+        print("Введите ссылку на фото, при наличии: ")
+        urlPhoto = readln()
+        urlPhoto +=" ".repeat(15-urlPhoto!!.length)
+    }
+
+    fun getStartDate(){
+        print("Введите дату принятия на работу: ")
+        startDate = readln()
+        startDate +=" ".repeat(10-startDate!!.length)
+
+    }
+}
---
+++ src/Task1/Main.kt
@@ -0,0 +1,102 @@
+package Task1
+
+import Task1.Data.EmployeeReposytory
+import Task1.Domain.UseCase.AddEmployee
+
+var i = 100
+
+
+var listEmployee = EmployeeReposytory()
+
+fun main(){
+    while (i!=0){
+        println("""
+        -----------------------------------------------------
+        |   Меню                                            |
+        |    1. Принять сотрудника                          |
+        |    2. Изменить данные сотрудника                  |
+        |    3. Уволить сотрудника                          |
+        |    4. Рассчитать премию                           |
+        |    5. Офильтровать по фамилии                     |
+        |    6. Офильтровать по дате устройства на работу   |
+        |    7. Офильтровать по стажу                       |
+        |    8. Отфильтровать по должности                  |
+        |    0. Выход из приложения                         |
+        -----------------------------------------------------
+            
+        """.trimIndent())
+        print("Введите номер пункта меню: ")
+        i = readln().toInt()
+
+        Thread.sleep(400)
+        print("Ответ системы: ")
+        when (i){
+            1 -> addEmployee()
+            2 -> updateEmployee()
+            3 -> deleteEmployee()
+            4 -> calculateBonus()
+            5 -> filterName()
+            6 -> filterStartDate()
+            7 -> filterWorkExperience()
+            0 -> {
+                exit()
+                break
+            }
+        }
+
+        Thread.sleep(1000)
+        println("Нажмите любую кнопку, чтобы подолжить")
+        readln()
+    }
+}
+// 1. Принять сотрудника
+fun addEmployee(){
+    var addEmployee = AddEmployee(listEmployee.toString())
+    addEmployee.getParametresNewEmployee()
+    println("Данные принятого сотрудника введены")
+    Thread.sleep(1000)
+    addEmployee.addNewEmployee(listEmployee)
+    println("Данные принятого сотрудника сохранены")
+    Thread.sleep(1000)
+}
+
+//2. Изменить данные сотрудника
+fun updateEmployee(){
+    println("Данные сотрудника изменены и обновлены")
+}
+
+//3. Уволить сотрудника
+fun deleteEmployee(){
+    println("Данные сотрудника удалены")
+}
+
+//4. Рассчитать премию
+fun calculateBonus(){
+    println("Премии рассчитаны")
+}
+
+//5. Офильтровать по фамилии
+fun filterName(){
+    println("Фильтрация по фамилии выполнена")
+}
+
+//6. Офильтровать по дате устройства на работу
+fun filterStartDate(){
+    println("Фильтрация по дате устройства на работу выполнена")
+}
+
+//7. Офильтровать стажу
+fun filterWorkExperience(){
+    println("Фильтрация по стажу выполнена")
+}
+
+//8. Отфильтровать по должности
+fun filterPosition(){
+    println("Фильтрация по должности выполнена")
+}
+
+//0. Вы выполнили выход из приложения
+fun exit(){
+    println("Вы выполнили выход из приложения")
+    Thread.sleep(1000)
+}
